4500b103eae75beb783a4f1d215649d1083dde91
complexluise <lhiguerac@unal.edu.co>
2024-05-06 09:01:12 -0500

    add plot for diversity by year

--- app/main.py
+++ app/main.py
@@ -21,21 +21,25 @@ years = np.arange(1995, 2023)
 
 
 @st.cache_data
-def load_data_diversity_countries() -> DataFrame:
+def load_data_diversity_countries() -> tuple[DataFrame, DataFrame]:
     ruta_datos = "data/processed_data/BACI_HS92_V202401b/"
-    df = pd.read_csv(
+    df_1 = pd.read_csv(
         ruta_datos + f"countries_diversity_HS92_V202401b.csv",
         sep=",")
-    df.drop(
+    df_1.drop(
         labels="Unnamed: 0",
         axis=1,
         inplace=True
     )
 
-    return df
+    df_2 = pd.read_csv(
+        ruta_datos + f"diversity_by_year_HS92_V202401b.csv",
+        sep=",")
+
+    return df_1, df_2
 
 
-df_diversidad_paises = load_data_diversity_countries()
+df_diversidad_paises, df_diversidad_tiempo = load_data_diversity_countries()
 
 
 def filter_dataframe(df, year):
@@ -54,13 +58,36 @@ def update_figure(year):
         font_size=16,
         title_font_family="Arial",
         title_font_color="black",
-        legend_title_font_color="black"
+        legend_title_font_color="black",
+        xaxis=dict(range=[0, 1200]),
+        yaxis=dict(range=[0, 1500])
     )
     fig.update_xaxes(title_font_family="Arial")
     return fig
 
 
-st.title("Diversidad de Exportaciones e Importaciones")
-year = st.selectbox("Seleccione un año:", years)
+st.title("Diversidad de Exportaciones e Importaciones por País en cada Año")
+year = st.slider("Seleccione un año:", min_value=min(years), max_value=max(years), value=min(years))
 fig = update_figure(year)
-st.plotly_chart(fig, use_container_width=True, height=1200)
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("Diversidad a lo largo de tiempo Latinoamerica y Mundo")
+df_melted = df_diversidad_tiempo.melt(id_vars=["tiempo"], var_name="Variable", value_name="Valor")
+
+# Creamos el gráfico interactivo
+fig_2 = px.line(df_melted, x="tiempo", y="Valor", color="Variable",
+              title="Diversidad de Importaciones y Exportaciones a lo Largo del Tiempo",
+              labels={"Valor": "Diversidad", "tiempo": "Año"})
+
+# Personalizamos el diseño del gráfico
+fig_2.update_layout(
+    template="plotly_white",
+    xaxis_title="Año",
+    yaxis_title="Diversidad",
+    legend_title="Indicadores"
+)
+
+st.plotly_chart(fig_2, use_container_width=True)
+
+
+
