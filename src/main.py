66f8d3d4014bb56b8554d434786a82f821dc07d8
complexluise <lhiguerac@unal.edu.co>
2024-05-12 22:44:18 -0500

    main with fix directory

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 """This is the main executable script for the CLI application."""
+from src.etl.etl_processor import ETLProcessor
+
+
+def upload_countries():
+    ETLProcessor.upload_countries("data/raw_data/world_bank_data/countries.csv")
 
 
 def main():
     """
     The main entry point of the application when run as a script.
     """
-    # Your code to initialize and start the application goes here
+    upload_products()
 
 
 if __name__ == "__main__":
