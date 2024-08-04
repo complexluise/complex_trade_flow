2522177a63e97b5ebfe1a695f900a22642ce15d8
complexluise <lhiguerac@unal.edu.co>
2024-08-04 18:13:22 -0500

    add region importer exporter

--- scripts/src/constants.py
+++ scripts/src/constants.py
@@ -5,6 +5,8 @@ class BACIColumnsTradeData(Enum):
     YEAR = "year"
     EXPORTER_ISO_CODE_3 = "exporter_iso_code_3"
     IMPORTER_ISO_CODE_3 = "importer_iso_code_3"
+    EXPORTER_REGION = "exporter_region"
+    IMPORTER_REGION = "importer_region"
     PRODUCT_CATEGORY_CODE = "product_category_code"
     MONEY = "money"
     MASS = "mass"
